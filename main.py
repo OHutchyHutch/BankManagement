cabdc995c25190f0dd05457c228315c7691baf47
Owen <>
2021-04-14 02:22:07 +0000

    Added choices and current balance

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ login = login.login(bankUsersClass)
 isLoggedIn, accessedUser = login.isRegistered()
 
 while(isLoggedIn):
-  action = str(input("\n\nWhat would you like to do? Deposit/Withdraw/Logout: "))
-  if (action == "Logout"):
+  print("\n\n---------------")
+  print("Current Balance: $" + str(accessedUser.getMoney()))
+  print("---------------")
+  print("\n1. Deposit\n2. Withdraw\n3. Logout")
+  action = str(input("\n\nWhat would you like to do:  "))
+  if (action == "Logout" or action == "3"):
     print("Goodbye, " + accessedUser.getName())
     isLoggedIn = False
     break
   amount = int(input ("How much? "))
-  if (action == "Deposit"):
+  if (action == "Deposit" or action == "1"):
     accessedUser.addMoney(amount)
-  elif (action == "Withdraw"):
+  elif (action == "Withdraw" or action == "2"):
     accessedUser.removeMoney(amount)
-    
-  print("Current Balance: " + str(accessedUser.getMoney()))
